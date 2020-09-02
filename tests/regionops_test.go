7aa7a28a7d0c19ee15063453f2818c336d61848e
Avinash Dongre (adongre) <Avinash.Dongre@quest.com>
2020-09-02 18:06:42 +0530

    print the error message properly in tests

--- tests/regionops_test.go
+++ tests/regionops_test.go
@@ -16,7 +16,7 @@ func (suite *GeodeTestSuite) DoRegionOps(regionName string, regionType RegionTyp
 			err2 := suite.Client.Region(regionName).Put(fmt.Sprintf("Key-%d", keyIndex),
 				fmt.Sprintf("Val-%d", keyIndex))
 			if err2 != nil {
-				suite.Fail("Failed during Put, Error = %v\n", err2)
+				suite.Fail(fmt.Sprintf("Failed during Put, Error = %v\n", err2))
 			}
 		}(&wgPut, i)
 	}
@@ -29,7 +29,7 @@ func (suite *GeodeTestSuite) DoRegionOps(regionName string, regionType RegionTyp
 			defer wg.Done()
 			value, err2 := suite.Client.Region(regionName).Get(fmt.Sprintf("Key-%d", keyIndex))
 			if err2 != nil {
-				suite.Fail("Failed during Get, Error = %v", err2)
+				suite.Fail(fmt.Sprintf("Failed during Get, Error = %v", err2))
 			}
 			if value != fmt.Sprintf("Val-%d", keyIndex) {
 				suite.Fail(fmt.Sprintf("Value Mismatch, Expected = %s Actual = %s",
@@ -43,7 +43,7 @@ func (suite *GeodeTestSuite) DoRegionOps(regionName string, regionType RegionTyp
 	if regionType == Replicate {
 		size, err := suite.Client.Region(regionName).Size()
 		if err != nil {
-			suite.Fail("Size Failed, Error = %v", err)
+			suite.Fail(fmt.Sprintf("Size Failed, Error = %v", err))
 		}
 
 		if 20 != size {
@@ -56,20 +56,20 @@ func (suite *GeodeTestSuite) DoRegionOps(regionName string, regionType RegionTyp
 		suite.Fail("KeySet Failed, Error = %v", err)
 	}
 	if len(keySet) != 20 {
-		suite.Fail("Did not return all the keys, Expected 20, Actual = %v", len(keySet))
+		suite.Fail(fmt.Sprintf("Did not return all the keys, Expected 20, Actual = %v", len(keySet)))
 	}
 
 	// PutIfAbsent
 	v, err := suite.Client.Region(regionName).PutIfAbsent("Key-1", "Value-New")
 	if err != nil {
-		suite.Fail("PutIfAbsent failed, Error = %v", err)
+		suite.Fail(fmt.Sprintf("PutIfAbsent failed, Error = %v", err))
 	}
 	if v != "Val-1" {
-		suite.Fail("PutIfAbsent failed, Expected %v ", v)
+		suite.Fail(fmt.Sprintf("PutIfAbsent failed, Expected %v ", v))
 	}
 	v, err = suite.Client.Region(regionName).PutIfAbsent("Key-100", "Value-New-100")
 	if err != nil {
-		suite.Fail("PutIfAbsent failed, Error = %v", err)
+		suite.Fail(fmt.Sprintf("PutIfAbsent failed, Error = %v", err))
 	}
 	if v != nil {
 		suite.Fail("PutIfAbsent should return nil")
@@ -85,10 +85,10 @@ func (suite *GeodeTestSuite) DoRegionOps(regionName string, regionType RegionTyp
 	}
 	putAll, err := suite.Client.Region(regionName).PutAll(kvs)
 	if err != nil {
-		suite.Fail("PutAll failed, Error =  %v", err)
+		suite.Fail(fmt.Sprintf("PutAll failed, Error =  %v", err))
 	}
 	if len(putAll) != 0 {
-		suite.Fail("PutAll Failed, Error = %v", err)
+		suite.Fail(fmt.Sprintf("PutAll Failed, Error = %v", err))
 	}
 
 	// GetAll
@@ -107,19 +107,19 @@ func (suite *GeodeTestSuite) DoRegionOps(regionName string, regionType RegionTyp
 		tokens := strings.Split(v.Key.(string), "-")
 		expectedValue := fmt.Sprintf("Val-PutAll-%s", tokens[len(tokens)-1])
 		if v.Value != expectedValue {
-			suite.Fail("GetAll Failed Expected Value = %v, Actual Value = %v",
-				expectedValue, v.Value)
+			suite.Fail(fmt.Sprintf("GetAll Failed Expected Value = %v, Actual Value = %v",
+				expectedValue, v.Value))
 		}
 	}
 	// Remove
 	key := "Key-PutAll-0"
 	err = suite.Client.Region(regionName).Remove(key)
 	if err != nil {
-		suite.Fail("Removed Failed, Error = %v", err)
+		suite.Fail(fmt.Sprintf("Removed Failed, Error = %v", err))
 	}
 	v, err = suite.Client.Region(regionName).Get(key)
 	if err != nil {
-		suite.Fail("Get Failed, Error = %v", err)
+		suite.Fail(fmt.Sprintf("Get Failed, Error = %v", err))
 	}
 	if v != nil {
 		suite.Fail("Remove failed, Get got the value")
@@ -129,14 +129,14 @@ func (suite *GeodeTestSuite) DoRegionOps(regionName string, regionType RegionTyp
 	if regionType == Replicate {
 		err = suite.Client.Region(regionName).Clear()
 		if err != nil {
-			suite.Fail("Clear failed, Error = %v", err)
+			suite.Fail(fmt.Sprintf("Clear failed, Error = %v", err))
 		}
 	}
 
 	if regionType == Replicate {
 		size, err := suite.Client.Region(regionName).Size()
 		if err != nil {
-			suite.Fail("Size Failed, Error = %v", err)
+			suite.Fail(fmt.Sprintf("Size Failed, Error = %v", err))
 		}
 		if 0 != size {
 			suite.Fail("size of region should be 20")
